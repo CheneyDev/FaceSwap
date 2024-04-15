b69e4b7ccfec77fa7a072026c17feb81d688311b
CheneyDev <cheneydev@gmail.com>
2024-04-15 17:43:04 +0800

    feat(frontend): Add new SwapPage component

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import {SwapPage} from "@/components/swap-page";
+
+export default function Home() {
+  return (
+      // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <SwapPage/>
+      // </main>
+  );
+}
